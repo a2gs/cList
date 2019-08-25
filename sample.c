7ea77a8da0993a79a6c940146e6c4a2a390263f7
Andre Augusto Giannotti Scota <andre.scota@gmail.com>
2019-08-25 01:10:34 -0300

    sample development 1

--- sample.c
+++ sample.c
@@ -25,14 +25,32 @@ int main(int argc, char *argv[])
 	clist_t cListCtrl;
 	float buffer[CIRCULAR_LIST_SIZE];
 	float node;
+	int i;
 
 	if(clist_create(&cListCtrl, buffer, CIRCULAR_LIST_SIZE, sizeof(float)) == CLIST_ERRO){
+		printf("Error clist_create()\n");
+		return(1);
 	}
 
 	node = 13.8;
 
-	clist_put(&cListCtrl, &node, copyNode);
+	for(i = 0; i < 50; i++){
+		if(clist_put(&cListCtrl, &node, copyNode) != CLIST_OK){
+			printf("Error clist_put() %d\n", i);
+			return(2);
+		}
+		printf("PUT: %f (i=%d)\n", node, i);
+		node += i;
+	}
 
 
+	for(i = 0; i < 30; i++){
+		if(clist_get(&cListCtrl, (void **)&node) != CLIST_OK){
+			printf("Error clist_get() %d\n", i);
+			return(2);
+		}
+		printf("GET: %f (i=%d)\n", node, i);
+
+	}
 	return(0);
 }
